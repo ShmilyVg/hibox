b570c18b81147cf334e621dabf4fa023159683c5
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-10 17:52:59 +0800

    新增两个页面

---
+++ pages/bind-device/bind-device.js
@@ -0,0 +1,13 @@
+// pages/bind-device/bind-device.js
+Page({
+
+    data: {},
+
+    onLoad: function (options) {
+
+    },
+    onGotUserInfo(e){
+        console.log('onGotUserInfo');
+    }
+
+})
---
+++ pages/connect-device/connect-device.js
@@ -0,0 +1,13 @@
+// pages/connect-device/connect-device.js
+Page({
+
+    data: {
+        backgroundColor:'#3E3E3E'
+    },
+
+    onLoad: function (options) {
+
+    },
+
+
+})
