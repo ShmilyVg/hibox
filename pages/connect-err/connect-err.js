691e85ce129705a8147c358297b1c21fa4dd223c
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-11 10:05:35 +0800

    连接失败页面完成

---
+++ pages/connect-err/connect-err.js
@@ -0,0 +1,16 @@
+// pages/connect-err/connect-err.js
+Page({
+
+    data: {
+        content: [
+            '手机未开启蓝牙',
+            '手机未授权微信获取定位权限',
+            '药盒离手机太远',
+            '未在药盒上短按按键确认'
+        ]
+    },
+    onLoad: function (options) {
+
+    }
+
+})
