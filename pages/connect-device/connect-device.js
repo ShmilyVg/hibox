257038966f7956ced7e32aa9ce6ccb50ae5a6fdc
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-11 14:12:33 +0800

    修改文字

--- pages/connect-device/connect-device.js
+++ pages/connect-device/connect-device.js
@@ -2,11 +2,29 @@
 Page({
 
     data: {
-        backgroundColor:'#3E3E3E'
+        backgroundColor: '#3E3E3E',
+        index: 0,
+        showContent: [
+            {
+                title: '将药盒靠近手机',
+                content: '正在努力的寻找药盒…',
+                backgroundColor: '#3E3E3E',
+                navigationColor: '#3E3E3E'
+            },
+            {
+                title: '药盒找到啦！',
+                content: '短按药盒按钮',
+                backgroundColor: 'linear-gradient(#66DABF, #008290)',
+                navigationColor: '#66DABF'
+            }
+        ]
     },
 
     onLoad: function (options) {
-
+        wx.setNavigationBarColor({
+            frontColor: '#ffffff',
+            backgroundColor: this.data.showContent[this.data.index].navigationColor,
+        })
     },
 
 
