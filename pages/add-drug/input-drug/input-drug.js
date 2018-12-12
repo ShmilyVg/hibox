7925a9195f18caea4e725a5bd9f0c9ee2216e477
liubiao <liubiao@vlifepaper.com>
2018-12-12 16:43:16 +0800

    完成输入药品名称页面

--- pages/add-drug/input-drug/input-drug.js
+++ pages/add-drug/input-drug/input-drug.js
@@ -6,34 +6,48 @@ import Protocol from "../../../modules/network/protocol";
 Page({
 
     data: {
-        stepStr: '',
-        name: '',
-        drugs: []
+        drugs: [],
+        name: ''
     },
     onLoad(options) {
         const {type, step, count} = options;
-        this.setData({
-            type,
-            step,
-            count,
-            stepStr: `${step}/${count}`
-        });
+        this.setData({type, step, count});
 
         Protocol.getDrugItems({type}).then(data => {
             const {result: drugs} = data;
             this.setData({drugs});
         });
     },
-    inputDrugNameEvent(e) {
+
+    nameChooseEvent(e) {
+        const {currentTarget: {dataset: {index}}} = e;
+        const obj = this.getAfterClearSelectedObj();
+        obj[`drugs[${index}].selected`] = true;
+        obj['name'] = this.data.drugs[index].name;
+        this.setData(obj);
+    },
+
+    lostFocusEvent(e) {
         console.log(e);
         const value = e.detail.value.trim();
         !!value && (this.data.name = value);
+    },
+
+    startInputEvent() {
+        this.setData(this.getAfterClearSelectedObj());
+    },
+
+    getAfterClearSelectedObj() {
+        let obj = {};
+        this.data.drugs.forEach((item, index) => item.selected && (obj[`drugs[${index}].selected`] = false));
+        return obj;
     },
 
     nextStep() {
         if (!!this.data.name) {
             HiNavigator.navigateToDrugNumberPage({...this.data});
         } else {
+            this.setData({name: ''});
             Toast.warn('请输入药名');
         }
     }
