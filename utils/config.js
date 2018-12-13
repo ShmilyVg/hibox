0f7b952f49c1f781561541f59309ef92ee4eb60e
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-13 12:00:57 +0800

    药品提醒列表基本完成。。

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,6 @@
 const PostUrl = 'https://backend.stage.hipee.cn/hipee-web-hibox/';
+const UploadUrl = 'https://backend.hipee.cn/hipee-upload/hibox/mp/upload/image.do';
 export {
     PostUrl,
+    UploadUrl
 };
