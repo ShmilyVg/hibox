357d6b14306b233c458fc2f9edecd3bc6ce6583c
liubiao <liubiao@vlifepaper.com>
2018-12-14 16:29:31 +0800

    设置药盒的UUID

--- modules/bluetooth/hi-box-bluetooth-manager.js
+++ modules/bluetooth/hi-box-bluetooth-manager.js
@@ -3,7 +3,7 @@ import HiBlueToothManager from "./hi-bluetooth-manager";
 export default class HiBoxBlueToothManager extends HiBlueToothManager{
     constructor() {
         super();
-        this.setUUIDs({services: ['6E400001-B5A3-F393-E0A9-E50E24DCCA9E']});//设置主Services方式如 this.setUUIDs({services: ['xxxx']})  xxxx为UUID全称，可设置多个
+        this.setUUIDs({services: ['6E400001-B5A3-F393-E0A9-E50E24DCCA9F']});//设置主Services方式如 this.setUUIDs({services: ['xxxx']})  xxxx为UUID全称，可设置多个
     }
     sendAlertTimeOperationProtocol({singleAlertData}) {
         this.bluetoothProtocol.sendAlertTime({singleAlertData});
