e93a498dd4fe39d1da21b05b072c230e18c90e0e
liubiao <liubiao@vlifepaper.com>
2018-12-14 15:02:43 +0800

    绑定状态

--- pages/connect-device/connect-device.js
+++ pages/connect-device/connect-device.js
@@ -1,4 +1,8 @@
 // pages/connect-device/connect-device.js
+import HiNavigator from "../../navigator/hi-navigator";
+
+const app = getApp();
+
 Page({
 
     data: {
@@ -27,9 +31,66 @@ Page({
             backgroundColor: this.data.showContent[this.data.index].navigationColor,
         });
         this.flickerHandle();
+        app.getBLEManager().connect();
+        app.setBLEListener({
+            bleStateListener: ({state}) => {
+                this.showResult({state});
+            },
+            receiveDataListener: ({finalResult, state}) => {
+                if (BlueToothState.GET_CONNECTED_RESULT_SUCCESS === state) {
+                    this.isBind = true;
+                    const {isConnected} = finalResult;
+                    app.getBLEManager().updateBLEStateImmediately({state: BlueToothState.CONNECTED_AND_BIND});
+                    isConnected && HiNavigator.switchToIndexPage({refresh: false});
+                }
+            }
+        });
+    },
+    onUnload() {
+        !this.isBind && app.getBLEManager().clearConnectedBLE();
     },
 
-    flickerHandle(){
+    reConnectEvent() {
+        app.getBLEManager().connect();
+    },
+
+    isBind: false,
+
+    getResultState({state}) {
+        switch (state) {
+            case BlueToothState.CONNECTING:
+                return {
+                    color: '#979797',
+                    text: '正在寻找您的设备\n请将设备开机并靠近手机',
+                    picPath: '../../images/device-bind/connecting.png'
+                };
+            case BlueToothState.UNAVAILABLE:
+            case BlueToothState.DISCONNECT:
+            case BlueToothState.UNBIND:
+                this.isBind = false;
+                app.getBLEManager().clearConnectedBLE();
+                return {
+                    color: '#979797',
+                    text: '绑定失败，请检查后重试',
+                    picPath: '../../images/device-bind/fail.png'
+                };
+            default:
+                return {
+                    color: '#FE5E01',
+                    text: '已找到您的设备\n短按设备上的按键确认绑定',
+                    picPath: '../../images/device-bind/connected.png'
+                };
+        }
+    },
+
+    showResult({state}) {
+        this.setData({
+            result: this.getResultState({state}),
+            showReConnected: state === BlueToothState.DISCONNECT || state === BlueToothState.UNAVAILABLE
+        });
+    },
+
+    flickerHandle() {
         let num = 0;
         let timer = setInterval(() => {
             this.setData({
