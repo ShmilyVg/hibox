00d5b037d22c31707cdc6a33c42e1c97baaffa83
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-18 17:54:59 +0800

    修改一个协议URL

--- modules/network/protocol.js
+++ modules/network/protocol.js
@@ -64,7 +64,7 @@ export default class Protocol {
         return Network.request({url: 'device/unbind'})
     }
 
-    static getDeviceBindList() {
-        return Network.request({url: 'device/bindList'})
+    static getDeviceBindInfo() {
+        return Network.request({url: 'device/bind/info'})
     }
 }
