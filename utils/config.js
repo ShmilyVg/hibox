c833e4f2a85b4cd2848cf1546dba166dbbcfa5ca
liubiao <liubiao@vlifepaper.com>
2018-12-24 17:31:49 +0800

    配置url

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,10 @@
+import {NetworkConfig} from "heheda-network";
+
 const PostUrl = 'https://backend.stage.hipee.cn/hipee-web-hibox/';
 const UploadUrl = 'https://backend.hipee.cn/hipee-upload/hibox/mp/upload/image.do';
+
+NetworkConfig.setConfig({url: PostUrl});
+
 export {
     PostUrl,
     UploadUrl
