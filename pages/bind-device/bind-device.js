bd3a9f87407fd58ead3a8db89c28a707858a6571
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-01-23 16:07:29 +0800

    回退代码

--- pages/bind-device/bind-device.js
+++ pages/bind-device/bind-device.js
@@ -12,7 +12,7 @@ Page({
 
     },
 
-    onShow(){
+    onShow() {
         wx.setNavigationBarColor({
             frontColor: '#ffffff',
             backgroundColor: '#3E3E3E',
@@ -20,54 +20,31 @@ Page({
     },
 
     onGotUserInfo(e) {
-        wx.getBluetoothAdapterState({
-            success(res) {
-                let state = res.available;
-                if (res.available){
-                    const {
-                        detail: {
-                            userInfo,
-                            encryptedData,
-                            iv
-                        }
-                    } = e;
-                    if (!!userInfo) {
-                        Toast.showLoading();
-                        Login.doRegister({
-                                userInfo,
-                                encryptedData,
-                                iv
-                            })
-                            .then(() => UserInfo.get())
-                            .then(({
-                                userInfo
-                            }) => !this.setData({
-                                userInfo
-                            }))
-                            .catch(() => setTimeout(Toast.warn, 0, '获取信息失败')).finally(function () {
-
-                                Toast.hiddenLoading();
-                                HiNavigator.navigateToConnectDevice();
-                            });
-                    }
-                } else {
-                    wx.showModal({
-                        title: 'TIPS',
-                        showCancel: false,
-                        content: '请开启手机蓝牙',
-                        confirmText: '我知道了',
-                        confirmColor: '#67D5B8',
-                    });
-                }
-            },fail(res){
-                wx.showModal({
-                    title: 'TIPS',
-                    showCancel: false,
-                    content: '请开启手机蓝牙',
-                    confirmText: '我知道了',
-                    confirmColor: '#67D5B8',
-                });
+        const {
+            detail: {
+                userInfo,
+                encryptedData,
+                iv
             }
-        })   
+        } = e;
+        if (!!userInfo) {
+            Toast.showLoading();
+            Login.doRegister({
+                    userInfo,
+                    encryptedData,
+                    iv
+                })
+                .then(() => UserInfo.get())
+                .then(({
+                    userInfo
+                }) => !this.setData({
+                    userInfo
+                }))
+                .catch(() => setTimeout(Toast.warn, 0, '获取信息失败')).finally(function () {
+
+                    Toast.hiddenLoading();
+                    HiNavigator.navigateToConnectDevice();
+                });
+        }
     }
 })
