272f27c9412c66f6f7c36dbe4ad9e710a3df5ff1
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-02-15 17:51:41 +0800

    改需求

--- pages/search-drug/search-drug.js
+++ pages/search-drug/search-drug.js
@@ -2,61 +2,72 @@ import Protocol from "../../modules/network/protocol";
 
 // pages/search-drug/search-drug.js
 Page({
-  data: {
-    drugNames: [],
-    inputName: "",
-  },
+    data: {
+        drugNames: [],
+        inputName: "",
+    },
 
-  onLoad: function (options) {
-    let name = getApp().globalData.addOrEditDrugObj.drugName;
-    if (name.length > 0) {
-      this.setData({
-        inputName: name
-      })
-      this.postDrugName(name);
-    }
-  },
+    onLoad: function (options) {
+        let name = getApp().globalData.addOrEditDrugObj.drugName;
+        if (name.length > 0) {
+            this.setData({
+                inputName: name
+            });
+            this.postDrugName(name);
+        }
+    },
 
-  postDrugName(name) {
-    if (name.length > 0) {
-      Protocol.getDrugSearch({
-        name: name
-      }).then(data => {
-        let drugNames = [];
-        data.result.map(item => {
-          drugNames.push(item.name)
-        })
+    postDrugName(name) {
+        if (name.length > 0) {
+            Protocol.getDrugSearch({
+                name: name
+            }).then(data => {
+                let drugNames = [];
+                data.result.map(item => {
+                    drugNames.push(item.name)
+                });
+                this.setData({
+                    drugNames: drugNames,
+                })
+            })
+        } else {
+            this.setData({
+                drugNames: []
+            })
+        }
+    },
+
+    inputDrug(e) {
+        let content = e.detail.value;
+        this.postDrugName(content);
         this.setData({
-          drugNames: drugNames,
+            inputContent: content
         })
-      })
-    } else {
-      this.setData({
-        drugNames: []
-      })
+    },
+
+    finishInput(e) {
+        console.log('e=', e);
+        getApp().globalData.addOrEditDrugObj.drugName = e.detail.value;
+        wx.navigateBack({
+            delta: 1
+        });
+    },
+
+    clickConfirm() {
+        this.backPage(this.data.inputContent);
+    },
+
+    clickCell(e) {
+        let index = e.currentTarget.dataset.index;
+        let name = this.data.drugNames[index];
+        this.backPage(name);
+    },
+
+    backPage(name){
+        console.log(name);
+        getApp().globalData.addOrEditDrugObj.drugName = name;
+        wx.navigateBack({
+            delta: 1
+        });
     }
-  },
-
-  inputDrug(e) {
-    let content = e.detail.value;
-    this.postDrugName(content);
-  },
-
-  finishInput(e) {
-    console.log('e=', e);
-    getApp().globalData.addOrEditDrugObj.drugName = e.detail.value;
-    wx.navigateBack({
-      delta: 1
-    });
-  },
-
-  clickCell(e) {
-    let index = e.currentTarget.dataset.index;
-    let name = this.data.drugNames[index];
-    console.log(name);
-    getApp().globalData.addOrEditDrugObj.drugName = name;
-    wx.navigateBack({
-      delta: 1
-    });
-  }
 })
