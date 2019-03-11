31a8dacf1a157441b95d937685452fa07b68e7fd
liubiao <liubiao@vlifepaper.com>
2019-03-11 11:42:58 +0800

    寻找药盒功能

--- pages/search-device/search-device.js
+++ pages/search-device/search-device.js
@@ -1,7 +1,7 @@
 // pages/search-device/search-device.js
 import Protocol from "../../modules/network/protocol";
 import HiNavigator from "../../navigator/hi-navigator";
-import {ConnectState} from "../../modules/bluetooth/bluetooth-state";
+import {ConnectState, ProtocolState} from "../../modules/bluetooth/bluetooth-state";
 import Toast from "../../view/toast";
 
 Page({
@@ -14,7 +14,16 @@ Page({
         let state = getApp().getLatestBLEState();
         this.setData({
             latestBLEState: state
-        })
+        });
+        this.isFind = false;
+        getApp().setBLEListener({
+            bleStateListener: ({state}) => {
+                if (state.protocolState === ProtocolState.FIND_DEVICE) {
+                    Toast.success('已找到药盒');
+                    this.isFind = true;
+                }
+            }
+        });
     },
 
     startSearch() {
@@ -26,17 +35,18 @@ Page({
             this.setData({
                 isSearching: true
             });
+            getApp().getBLEManager().sendFindDeviceProtocol()
             let timer = setInterval(() => {
-                this.data.num > 1 && getApp().getBLEManager().sendFindDeviceProtocol();
                 this.setData({
                     num: --this.data.num
                 });
-                if (this.data.num <= 0) {
+                if (this.data.num <= 0 || this.isFind) {
                     this.setData({
                         num: 10,
                         isSearching: false
                     });
                     clearInterval(timer);
+                    this.isFind = false;
                 }
             }, 1000);
         } else {
