87638043e3469d932b7c44a641425b6ee08cbff3
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-03-20 15:10:08 +0800

    ui修改

--- pages/drug-info/drug-info.js
+++ pages/drug-info/drug-info.js
@@ -19,34 +19,104 @@ Page({
     },
 
     onLoad: function (options) {
-        // Protocol.getDrugCode({code: '6943118000248'}).then(data => {
+        // Protocol.getDrugCode({code: '6920312611029'}).then(data => {
+        //     let items = data.result.items;
+        //     let itemsTop = [], itemsBottom = [];
+        //     items.map(item => {
+        //         if (item.imgUrl) {
+        //             itemsTop.push(this.handleItem(true, item));
+        //         } else {
+        //             itemsBottom.push(this.handleItem(false, item));
+        //         }
+        //     });
         //     this.setData({
-        //         data: data.result
+        //         data: data.result,
+        //         itemsTop: itemsTop,
+        //         itemsBottom: itemsBottom
         //     })
         // })
 
         let drugInfo = getApp().globalData.drugInfo;
+        console.log('drugInfo====>',drugInfo);
+        let items = drugInfo.result.items;
+        let itemsTop = [], itemsBottom = [];
+        items.map(item => {
+            if (item.imgUrl) {
+                itemsTop.push(this.handleItem(true, item));
+            } else {
+                itemsBottom.push(this.handleItem(false, item));
+            }
+        });
+        this.setData({
+            data: drugInfo.result,
+            itemsTop: itemsTop,
+            itemsBottom: itemsBottom
+        });
 
         this.setData({
             data: drugInfo.result
         });
 
-        console.log('addOrEditDrugObj=',getApp().globalData.addOrEditDrugObj);
+        console.log('addOrEditDrugObj=', getApp().globalData.addOrEditDrugObj);
+    },
+
+    handleItem(isTop, item) {
+        let num = isTop ? 72 : 36;
+        if (item.content.length > num) {
+            item.showArr = true;
+            item.contentLong = item.content;
+            item.contentShort = item.content.substring(0, num) + '...';
+            item.content = item.contentShort;
+        } else {
+            item.showArr = false;
+        }
+        return item;
     },
 
     drugRemind() {
-
-        if(this.data.data.cycle != null){
-            let times  = this.data.ruler[this.data.data.cycle];
+        if (this.data.data.cycle != null) {
+            let times = this.data.ruler[this.data.data.cycle];
             let value = [];
-            for (let i = 0; i < times.length; i++){
-                value[i] = {number:this.data.data.dosage,remind_time:times[i]}
+            for (let i = 0; i < times.length; i++) {
+                value[i] = {number: this.data.data.dosage, remind_time: times[i]}
             }
 
             getApp().globalData.addOrEditDrugObj.items = value;
         }
 
 
-        HiNavigator.navigateToDrugNumberPage({drugName: this.data.data.drugName, step: 2, count: 3,code:this.data.data.code});
+        HiNavigator.navigateToDrugNumberPage({
+            drugName: this.data.data.drugName,
+            step: 2,
+            count: 3,
+            code: this.data.data.code
+        });
+    },
+
+    clickTopArr(e) {
+        let index = e.currentTarget.dataset.index;
+        this.data.itemsTop[index] = this.handleClick(index, this.data.itemsTop);
+        this.setData({
+            itemsTop: this.data.itemsTop
+        })
+    },
+
+    clickBottomArr(e) {
+        let index = e.currentTarget.dataset.index;
+        this.data.itemsBottom[index] = this.handleClick(index, this.data.itemsBottom);
+        this.setData({
+            itemsBottom: this.data.itemsBottom
+        })
+    },
+
+    handleClick(index, items) {
+        let item = items[index];
+        if (item.content === item.contentShort) {
+            item.content = item.contentLong;
+        } else {
+            item.content = item.contentShort;
+        }
+        return item;
     }
+
 })
