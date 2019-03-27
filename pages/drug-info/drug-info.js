6745dace762b39290257901fad5576428f58b97f
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-03-27 14:18:11 +0800

    逻辑修改

--- pages/drug-info/drug-info.js
+++ pages/drug-info/drug-info.js
@@ -1,58 +1,51 @@
 // pages/drug-info/drug-info.js
 import Protocol from "../../modules/network/protocol";
 import HiNavigator from "../../navigator/hi-navigator";
+import * as tools from "../../utils/tools";
+
+let pageDebug = false;
 
 Page({
-
     data: {
-        ruler: {
-            1: ['08:00'],
-            2: ['08:00', '21:00'],
-            3: ["08:00", "13:00", "21:00"],
-            4: ["08:00", "13:00", "18:00", "21:00"],
-            5: ["08:00", "12:00", "15:00", "18:00", "21:00"],
-            6: ["08:00", "11:00", "14:00", "17:00", "19:00", "21:00"],
-            7: ["08:00", "10:30", "13:00", "15:30", "17:00", "19:30", "21:00"],
-            8: ["08:00", "10:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00"],
-            9: ["08:00", "09:30", "11:00", "12:30", "14:00", "15:30", "17:00", "18:30", "21:00"],
-        }
     },
+    onLoad() {
+        this.data.ruler = tools.getRulerTime();
+        if (pageDebug) {
+            Protocol.getDrugCode({code: '6920312611029'}).then(data => {
+                let items = data.result.items;
+                let itemsTop = [], itemsBottom = [];
+                items.map(item => {
+                    if (item.imgUrl) {
+                        itemsTop.push(this.handleItem(true, item));
+                    } else {
+                        itemsBottom.push(this.handleItem(false, item));
+                    }
+                });
+                this.setData({
+                    data: data.result,
+                    itemsTop: itemsTop,
+                    itemsBottom: itemsBottom
+                })
+            })
+        } else {
+            let drugInfo = getApp().globalData.drugInfo;
+            let items = drugInfo.result.items;
+            let itemsTop = [], itemsBottom = [];
+            items.map(item => {
+                if (item.imgUrl) {
+                    itemsTop.push(this.handleItem(true, item));
+                } else {
+                    itemsBottom.push(this.handleItem(false, item));
+                }
+            });
+            this.setData({
+                data: drugInfo.result,
+                itemsTop: itemsTop,
+                itemsBottom: itemsBottom
+            });
 
-    onLoad: function (options) {
-        // Protocol.getDrugCode({code: '6920312611029'}).then(data => {
-        //     let items = data.result.items;
-        //     let itemsTop = [], itemsBottom = [];
-        //     items.map(item => {
-        //         if (item.imgUrl) {
-        //             itemsTop.push(this.handleItem(true, item));
-        //         } else {
-        //             itemsBottom.push(this.handleItem(false, item));
-        //         }
-        //     });
-        //     this.setData({
-        //         data: data.result,
-        //         itemsTop: itemsTop,
-        //         itemsBottom: itemsBottom
-        //     })
-        // })
-
-        let drugInfo = getApp().globalData.drugInfo;
-        let items = drugInfo.result.items;
-        let itemsTop = [], itemsBottom = [];
-        items.map(item => {
-            if (item.imgUrl) {
-                itemsTop.push(this.handleItem(true, item));
-            } else {
-                itemsBottom.push(this.handleItem(false, item));
-            }
-        });
-        this.setData({
-            data: drugInfo.result,
-            itemsTop: itemsTop,
-            itemsBottom: itemsBottom
-        });
-
-        console.log('addOrEditDrugObj=', getApp().globalData.addOrEditDrugObj);
+            console.log('addOrEditDrugObj=', getApp().globalData.addOrEditDrugObj);
+        }
     },
 
     handleItem(isTop, item) {
@@ -107,11 +100,7 @@ Page({
 
     handleClick(index, items) {
         let item = items[index];
-        if (item.content === item.contentShort) {
-            item.content = item.contentLong;
-        } else {
-            item.content = item.contentShort;
-        }
+        item.content = item.content === item.contentShort ? item.contentLong : item.contentShort;
         item.arrIsNor = !item.arrIsNor;
         return item;
     }
