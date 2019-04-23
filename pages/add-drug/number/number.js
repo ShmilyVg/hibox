69363fc67db8dd72417ec3a2dc6fb42affeb56bf
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-04-23 14:48:08 +0800

    服用说明页面

--- pages/add-drug/number/number.js
+++ pages/add-drug/number/number.js
@@ -223,5 +223,11 @@ Page({
         DrugRuler.sendAlertTimeDataToBLE({singleAlertData}).catch(() => {
             Toast.warn('设置失败请重试');
         });
+    },
+
+    threePoint() {
+        wx.navigateTo({
+            url: '../../use-instructions/use-instructions'
+        })
     }
 });
